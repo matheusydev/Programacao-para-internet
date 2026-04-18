9f168fec2e386e694d92744766cf9d43c45a73e2
Matheusydev <matheusylanaraujo@gmail.com>
2026-04-17 22:28:16 -0300

    :white_check_mark: feat: finaliza atividade 3 dos exercícios sobre DOM 01

--- Atividade D.O.M 1/scripts.js
+++ Atividade D.O.M 1/scripts.js
@@ -9,9 +9,15 @@ function validar() {
 }
 
 function alterarParagrafos() {
-    let paragrafos = document.getElementsByTagName("p");
+    let paragrafos = document.getElementById("alterar").getElementsByTagName("p");
  
     for (let i = 0; i < paragrafos.length; i++) {
         paragrafos[i].innerHTML = "Parágrafo " + (i + 1) + " alterado!";
     }
+}
+
+function contarParagrafos() {
+    let paragrafos = document.getElementById("contar").children;
+
+    document.getElementById("resultado").innerHTML = "Total de parágrafos: " + paragrafos.length;
 }

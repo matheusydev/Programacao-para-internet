88591abe6da27540b5179d69ea238514aa63d574
Matheusydev <matheusylanaraujo@gmail.com>
2026-04-17 22:07:36 -0300

    :white_check_mark: feat: finaliza questão 2 dos exercícios sobre DOM 01

--- Atividade D.O.M 1/scripts.js
+++ Atividade D.O.M 1/scripts.js
@@ -1,9 +1,17 @@
 function validar() {
-  let nome = document.getElementById("nome").value;
+    let nome = document.getElementById("nome").value;
 
-  if (nome === "") {
-    document.getElementById("erro").innerHTML = "Preencha o campo!";
-  } else {
-    document.getElementById("erro").innerHTML = "Tudo certo!";
-  }
+    if (nome === "") {
+        document.getElementById("erro").innerHTML = "Preencha o campo!";
+    } else {
+        document.getElementById("erro").innerHTML = "Campo preenchido!";
+    }
+}
+
+function alterarParagrafos() {
+    let paragrafos = document.getElementsByTagName("p");
+ 
+    for (let i = 0; i < paragrafos.length; i++) {
+        paragrafos[i].innerHTML = "Parágrafo " + (i + 1) + " alterado!";
+    }
 }
